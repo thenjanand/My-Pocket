7a14f699c32acc83c57cddeb96854832c1c7f13a
Njanand-xenon <njanand.xenon@gmail.com>
2022-05-13 21:47:35 +0530

    Project uploaded

---
+++ src/components/footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// const year=new Date().getFullYear();
+
+function Footer() {
+  return (
+    <div>
+      <footer className="blue lighten-4 page-footer">
+        <div className="blue lighten-2 footer-copyright">
+          <div className="container white-text text-lighten-2 ">
+            <center>
+              <a className="white-text text-lighten-2 middle" href="">
+                Made with ♥ by <b>Neeraj Anand</b>
+              </a>
+            </center>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
