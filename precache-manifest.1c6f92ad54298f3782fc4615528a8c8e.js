6618c28f064a7d9f178f679c939df3453b1f2fa6
Njanand-xenon <njanand.xenon@gmail.com>
2022-05-13 23:18:53 +0530

    Updates

---
+++ precache-manifest.1c6f92ad54298f3782fc4615528a8c8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4d07c38f7567dee4c81e678913ebe9",
+    "url": "/My-Pocket/index.html"
+  },
+  {
+    "revision": "f8442bd106cc128d7f87",
+    "url": "/My-Pocket/static/js/2.5f3b39fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Pocket/static/js/2.5f3b39fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bef1c3141543f25ddb06",
+    "url": "/My-Pocket/static/js/main.dfcb51c1.chunk.js"
+  },
+  {
+    "revision": "61ee4cbfc38ea455047a",
+    "url": "/My-Pocket/static/js/runtime-main.1ef558b3.js"
+  }
+]);
